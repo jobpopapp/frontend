54bd35978e0adea172dd77b9d492a1358b8c034f
ugfire <ugfirecompany@gmail.com>
2025-07-30 23:04:31 +0300

    APP WORKS WELL, ADDED BILLING ADDRESS CRUD 30-JULY-205

--- src/app/pages/billing-address/billing-address.component.ts
+++ src/app/pages/billing-address/billing-address.component.ts
@@ -1,54 +1,79 @@
-import { Component, OnInit } from '@angular/core';
+// (removed duplicate getDefaultBillingAddress)
+import { Component, OnInit, ChangeDetectorRef, NgZone } from '@angular/core';
 import { BillingService, BillingAddress } from '../../services/billing.service';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { SidebarComponent } from '../../components/layout/sidebar/sidebar.component';
 import { NavbarComponent } from '../../components/layout/navbar/navbar.component';
-import { finalize } from 'rxjs/operators';
 import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-billing-address',
   templateUrl: './billing-address.component.html',
   styleUrls: ['./billing-address.component.scss'],
-    standalone: true,
-    imports: [CommonModule, FormsModule, SidebarComponent, NavbarComponent]
+  standalone: true,
+  imports: [CommonModule, FormsModule, SidebarComponent, NavbarComponent]
 })
 export class BillingAddressComponent implements OnInit {
-  billingAddress: BillingAddress = {
-    id: undefined,
-    email_address: '',
-    phone_number: '',
-    country_code: '',
-    first_name: '',
-    last_name: '',
-    line_1: '',
-    city: '',
-    state: '',
-    postal_code: '',
-    zip_code: ''
-  };
+  billingAddress: BillingAddress = this.getDefaultBillingAddress();
+
   loading = false;
   success = false;
   error: string | null = null;
 
-  constructor(private billingService: BillingService) {}
+  private getDefaultBillingAddress(): BillingAddress {
+    return {
+      id: undefined,
+      email_address: '',
+      phone_number: '',
+      country_code: '',
+      first_name: '',
+      last_name: '',
+      line_1: '',
+      city: '',
+      state: '',
+      postal_code: '',
+      zip_code: ''
+    };
+  }
+
+  constructor(private billingService: BillingService, private cdr: ChangeDetectorRef, private ngZone: NgZone) {
+    console.log('[BillingAddressComponent] Constructor - loading:', this.loading);
+  }
 
   ngOnInit() {
+    console.log('[BillingAddressComponent] ngOnInit - Before setting loading to true:', this.loading);
     this.loading = true;
+    console.log('[BillingAddressComponent] ngOnInit - After setting loading to true:', this.loading);
     this.billingService.getBillingAddress().subscribe({
       next: (address) => {
         if (address) {
           this.billingAddress = address;
+        } else {
+          this.billingAddress = this.getDefaultBillingAddress();
         }
-        this.loading = false;
+        console.log('[BillingAddressComponent] ngOnInit - getBillingAddress next - Before setting loading to false:', this.loading);
+        this.ngZone.run(() => {
+          this.loading = false;
+          console.log('[BillingAddressComponent] ngOnInit - getBillingAddress next - After setting loading to false (inside ngZone):', this.loading);
+        });
       },
-      error: () => { this.loading = false; }
+      error: () => {
+        this.billingAddress = this.getDefaultBillingAddress();
+        console.log('[BillingAddressComponent] ngOnInit - getBillingAddress error - Before setting loading to false:', this.loading);
+        this.ngZone.run(() => {
+          this.loading = false;
+          console.log('[BillingAddressComponent] ngOnInit - getBillingAddress error - After setting loading to false (inside ngZone):', this.loading);
+        });
+      }
     });
   }
 
   saveAddress() {
+    // Always ensure billingAddress is never null after save
+    console.log('[BillingAddressComponent] saveAddress - Before setting loading to true:', this.loading);
     this.loading = true;
+    console.log('[BillingAddressComponent] saveAddress - After setting loading to true:', this.loading);
 
     // Convert country name to 2-letter ISO code if necessary
     const addressToSend = { ...this.billingAddress };
@@ -57,25 +82,21 @@ export class BillingAddressComponent implements OnInit {
     }
 
     console.log('[BillingAddressComponent] Saving billing address:', addressToSend);
-    this.billingService.saveBillingAddress(addressToSend).pipe(
-      finalize(() => this.loading = false)
-    ).subscribe({
-      next: (updated) => {
-        this.billingAddress = updated;
-        Swal.fire({
-          icon: 'success',
-          title: 'Success!',
-          text: 'Billing address saved successfully.',
-          timer: 3000,
-          showConfirmButton: false
+    this.billingService.saveBillingAddress(addressToSend).subscribe({
+      next: () => {
+        this.ngZone.run(() => {
+          this.loading = false;
+          this.success = true;
+          this.error = null;
+          this.billingAddress = this.getDefaultBillingAddress();
         });
       },
-      error: (err) => {
-        console.error('[BillingAddressComponent] Save error:', err);
-        Swal.fire({
-          icon: 'error',
-          title: 'Error!',
-          text: err?.error?.message || 'Failed to save billing address. Please try again.'
+      error: (err: any) => {
+        this.ngZone.run(() => {
+          this.loading = false;
+          this.success = false;
+          this.error = err?.error?.message || 'Failed to save billing address.';
+          if (!this.billingAddress) this.billingAddress = this.getDefaultBillingAddress();
         });
       }
     });
