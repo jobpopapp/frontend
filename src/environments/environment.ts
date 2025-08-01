15bcd8d5912da6fa23d49e2aad1d0253ff853335
ugfire <ugfirecompany@gmail.com>
2025-08-01 07:23:02 +0300

    VERSION 1 31-JULY-2025- added angular guard

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:3000/api',
+  apiUrl: 'http://jobpop.grealm.org/api',
   healthCheckUrl: 'http://localhost:3001/health',
   appName: 'JobPop Company Portal',
   version: '1.0.0'
