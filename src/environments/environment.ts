3f742422d62256b78abb361aad59fbb8d807363c
ugfire <ugfirecompany@gmail.com>
2025-08-08 16:36:52 +0300

    BUILD FOR PRODUCTION - VERSION 1 - 8 AUG 2025

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,9 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:3000/api',
-  healthCheckUrl: 'http://localhost:3001/health',
+  //apiUrl: 'http://localhost:3000/api',
+  apiUrl:'https://api.jobpop.app/api',
+  //healthCheckUrl: 'http://localhost:3001/health',
+  healthCheckUrl: 'https://api.jobpop.app/health',
   appName: 'JobPop Company Portal',
   version: '1.0.0'
 };
